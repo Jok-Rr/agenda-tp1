b429fce2dee2b1f14bdc4d2bf532363e136b0a41
Tom Bost <tom.bost.pro@gmail.com>
2022-05-11 10:46:20 +0200

    First Commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+  #[Route('/', name: 'home')]
+  public function index(): Response
+  {
+    $arrayData = [
+      ['id' => 1, 'firstname' => 'Tom', 'lastname' => 'Bost', 'phone' => '0641970576'],
+      ['id' => 2, 'firstname' => 'Serge', 'lastname' => 'Lema', 'phone' => '0658965320'],
+      ['id' => 3, 'firstname' => 'Florian', 'lastname' => 'Barraud', 'phone' => '0641156425'],
+
+    ];
+
+    return $this->render('home/home.html.twig', ['data' => $arrayData]);
+  }
+
+  #[Route('/view/{id}', name: 'contact_view')]
+  public function view($id): Response
+  {
+    return $this->render('home/contact.html.twig');
+  }
+}
